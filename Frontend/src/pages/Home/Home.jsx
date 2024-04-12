baa620eda9c13e773b2d8406e52a74ce507a5a36
Javi <javi@jsrois.net>
2024-04-12 09:56:03 +0200

    Página Home

---
+++ Frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+import { Header } from '../../components/Header/Header';
+import { Banner } from '../../components/Banner/Banner';
+import { NewsLetter } from '../../components/NewsLetter/NewsLetter';
+import { Footer } from '../../components/Footer/Footer';
+
+export const Home = () => <div>
+    <Header />
+    <Banner />
+    <NewsLetter />
+    <Footer />
+</div>
+
+export default Home;
